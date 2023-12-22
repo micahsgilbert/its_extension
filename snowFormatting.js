0f2a66498a28fc2808051edbe2c7a5e75a7def7b
Ari Stassinopoulos <stassinopoulosari@gmail.com>
2023-12-22 14:55:29 -0800

    Version 1.5.1: Add buttons to the 'More options' menu

--- snowFormatting.js
+++ snowFormatting.js
@@ -1,62 +1,78 @@
 (() => {
   const logging = false,
+    // If the user is using a Mac, we'll need to change the super key
     isMacUser = navigator.userAgent.indexOf("Mac") != -1,
+    // Persistence for confirming solution proposed
     confirmProposingSolution =
       localStorage["its-extension-confirm-proposing-solution"] == "1",
     log = (str) => {
       if (logging) console.log(str);
     },
+    sysID = NOW !== undefined && NOW.sysId !== undefined ? NOW.sysId : null,
+    caseNumberID = "sys_readonly.sn_customerservice_case.number",
+    caseNumber =
+      document.getElementById(caseNumberID) !== null
+        ? document.getElementById(caseNumberID).value
+        : null,
+    // Dictionary of text replacements
+    replacements = {
+      hyperlink: (() => {
+        const startCode = '[code]<a href="" target="_blank">';
+        return {
+          startCode: startCode,
+          endCode: "</a>[/code]",
+          // Move the cursor into the href attribute
+          selectionEnd: startCode.indexOf('"') + 1,
+        };
+      })(),
+      bold: {
+        startCode: "[code]<b>",
+        endCode: "</b>[/code]",
+      },
+      italic: {
+        startCode: "[code]<i>",
+        endCode: "</i>[/code]",
+      },
+      image: {
+        startCode: `[code]<img src="`,
+        endCode: `" width=600px />[/code]`,
+      },
+      // Numbered list
+      orderedList: {
+        startCode: "[code]\n<ol>\n<li>\n",
+        endCode: "\n</li>\n</ol>\n[/code]",
+      },
+      // Bullet list
+      unorderedList: {
+        startCode: "[code]\n<ul>\n<li>\n",
+        endCode: "\n</li>\n</ul>\n[/code]",
+      },
+      // Inline code block
+      code: {
+        startCode:
+          "[code]<code style='display: inline-block; border: 0.5px solid #BBBBBB; border-radius: 1px; background-color: #E5E5E5; padding: 5px; margin-left: 3px; margin-right: 2px;'>",
+        endCode: "</code>[/code]",
+      },
+      // Blockquote block
+      blockquote: {
+        startCode:
+          "[code]<blockquote style='border-left: 3px solid #00629B; padding: 1em;'>\n[/code]",
+        endCode: "[code]\n</blockquote>[/code]",
+      },
+      listItem: {
+        startCode: "<li>\n",
+        endCode: "\n</li>",
+      },
+      paragraph: {
+        startCode: "[code]<p>\n",
+        endCode: "\n</p>[/code]",
+      },
+    },
+    // Command to commit text replacement
     editHighlighted = function (command, selection) {
       log("in command");
-      const replacements = {
-        hyperlink: (() => {
-          const startCode = '[code]<a href="" target="_blank">';
-          return {
-            startCode: startCode,
-            endCode: "</a>[/code]",
-            selectionEnd: startCode.indexOf('"') + 1,
-          };
-        })(),
-        bold: {
-          startCode: "[code]<b>",
-          endCode: "</b>[/code]",
-        },
-        italic: {
-          startCode: "[code]<i>",
-          endCode: "</i>[/code]",
-        },
-        image: {
-          startCode: `[code]<img src="`,
-          endCode: `" width=600px />[/code]`,
-        },
-        orderedList: {
-          startCode: "[code]\n<ol>\n<li>\n",
-          endCode: "\n</li>\n</ol>\n[/code]",
-        },
-        unorderedList: {
-          startCode: "[code]\n<ul>\n<li>\n",
-          endCode: "\n</li>\n</ul>\n[/code]",
-        },
-        code: {
-          startCode:
-            "[code]<code style='display: inline-block; border: 0.5px solid #BBBBBB; border-radius: 1px; background-color: #E5E5E5; padding: 5px; margin-left: 3px; margin-right: 2px;'>",
-          endCode: "</code>[/code]",
-        },
-        blockquote: {
-          startCode:
-            "[code]<blockquote style='border-left: 3px solid #00629B; padding: 1em;'>\n[/code]",
-          endCode: "[code]\n</blockquote>[/code]",
-        },
-        listItem: {
-          startCode: "<li>\n",
-          endCode: "\n</li>",
-        },
-        paragraph: {
-          startCode: "[code]<p>\n",
-          endCode: "\n</p>[/code]",
-        },
-      };
 
+      // Get focus element, character bounds
       const textArea = selection.focusNode.querySelector("textarea,input"),
         start = textArea.selectionStart,
         end = textArea.selectionEnd,
@@ -66,14 +82,18 @@
       var startCode,
         endCode,
         selectionEnd = -1;
+
+      // Make sure that the command is valid
       if (!Object.keys(replacements).includes(command)) {
         return;
       }
 
+      // Get replacement, start and end codes
       const replacement = replacements[command];
       startCode = replacement.startCode;
       endCode = replacement.endCode;
 
+      //  If the replacement has a selectionEnd, use that info, otherwise calculate it.
       if (Object.keys(replacement).includes("selectionEnd")) {
         selectionEnd = start + replacement.selectionEnd;
       } else {
@@ -82,11 +102,14 @@
             ? end + startCode.length + endCode.length
             : start + startCode.length;
       }
+
+      // Insert text
       const insertedText =
         startCode +
         textArea.value.substring(start, end).trim() +
         endCode +
         optionalSpace;
+      // This is deprecated but it's the only way to insert text
       document.execCommand("insertText", false, insertedText);
       setTimeout(() => {
         textArea.blur();
@@ -123,16 +146,16 @@
           }
         }
 
-        if (
-          letter == "c" &&
-          shiftDown == true &&
-          NOW !== undefined &&
-          NOW.sysId !== undefined
-        ) {
-          navigator.clipboard.writeText(NOW.sysId);
-          e.preventDefault();
-          return;
-        }
+        // if (
+        //   letter == "c" &&
+        //   shiftDown == true &&
+        //   sysID !== null &&
+        //   selection. // Don't interfere with <code> command
+        // ) {
+        //   navigator.clipboard.writeText(sysID);
+        //   e.preventDefault();
+        //   return;
+        // }
 
         if (
           selection.focusNode !== selection.anchorNode ||
@@ -164,7 +187,6 @@
         !selection.focusNode.querySelector("textarea")
       )
         return;
-
       const originalPasteText = (
         e.clipboardData || window.clipboardData
       ).getData("text");
@@ -179,17 +201,8 @@
           `${NOW.user.firstName} ${NOW.user.lastName}`,
         );
       }
-      if (
-        pasteText.includes("CASE") &&
-        document.getElementById(
-          "sys_readonly.sn_customerservice_case.number",
-        ) !== null
-      ) {
-        pasteText = pasteText.replaceAll(
-          /CASE/g,
-          document.getElementById("sys_readonly.sn_customerservice_case.number")
-            .value,
-        );
+      if (pasteText.includes("CASE") && caseNumber !== null) {
+        pasteText = pasteText.replaceAll(/CASE/g, caseNumber);
       }
       if (pasteText.includes("received from: no-reply@duosecurity.com")) {
         pasteText = pasteText.replaceAll(
@@ -197,22 +210,31 @@
           "",
         );
       }
-      if (
-        pasteText.includes("SYSID") &&
-        NOW !== undefined &&
-        NOW.sysId !== undefined
-      ) {
-        pasteText = pasteText.replaceAll(/SYSID/g, NOW.sysId);
+      if (pasteText.includes("SYSID") && sysID !== null) {
+        pasteText = pasteText.replaceAll(/SYSID/g, sysID);
+      }
+
+      if (pasteText.endsWith("?t=large")) {
+        pasteText = pasteText.slice(0, -1 * "?t=large".length);
       }
 
       if (pasteText !== originalPasteText) {
-        // https://developer.mozilla.org/en-US/docs/Web/API/Element/paste_event
-        // selection.deleteFromDocument();
-        // selection.getRangeAt(0).insertNode(document.createTextNode(pasteText));
-        // selection.collapseToEnd();
         document.execCommand("insertText", false, pasteText);
         e.preventDefault();
       }
+    },
+    createButtonRow = (id, action, text, classes) => {
+      const $rowElement = document.createElement("div"),
+        $buttonElement = document.createElement("button");
+      $rowElement.classList.add("row");
+      $buttonElement.id = id;
+      $buttonElement.innerText = text;
+      $buttonElement.onclick = action;
+      if (classes !== undefined) {
+        classes.forEach((className) => $buttonElement.classList.add(className));
+      }
+      $rowElement.appendChild($buttonElement);
+      return $rowElement;
     };
 
   //[shift, letter]
@@ -241,6 +263,57 @@
         )
         .forEach((button) => (button.innerText = "Save and Slay"));
     }
+    if (
+      caseNumber !== null &&
+      sysID !== null &&
+      document.getElementById("moreOptionsContainer") !== undefined &&
+      document
+        .getElementById("moreOptionsContainer")
+        .querySelector(".popover-body") !== undefined
+    ) {
+      const $moreOptionsPopover = document
+          .getElementById("moreOptionsContainer")
+          .querySelector(".popover-body"),
+        $lastRow = [].slice.call(
+          $moreOptionsPopover.querySelectorAll(".row"),
+          -1,
+        )[0];
+      if ($lastRow !== undefined) {
+        const buttons = [
+          createButtonRow(
+            "button-copy-customer-link",
+            () => {
+              navigator.clipboard.writeText(
+                `https://support.ucsd.edu/services?id=ticket&table=sn_customerservice_case&sys_id=${sysID}`,
+              );
+            },
+            "Copy Customer Link",
+            ["icon-copy"],
+          ),
+          createButtonRow(
+            "button-copy-agent-link",
+            () => {
+              navigator.clipboard.writeText(
+                `https://support.ucsd.edu/nav_to.do?uri=task.do?sysparm_query=number=${caseNumber}`,
+              );
+            },
+            "Copy Agent Link",
+            ["icon-copy"],
+          ),
+          createButtonRow(
+            "button-copy-sys-id",
+            () => {
+              navigator.clipboard.writeText(sysID);
+            },
+            "Copy System ID",
+            ["icon-copy"],
+          ),
+        ];
+        buttons.toReversed().forEach(($button) => {
+          $lastRow.after($button);
+        });
+      }
+    }
     if (confirmProposingSolution) {
       [].slice
         .call(
@@ -256,13 +329,6 @@
               button.getAttribute("onclick"),
           );
         });
-
-      // const originalGsftSubmit = window.gsftSubmit ?? function() { };
-      // window.gsftSubmit = function() {
-      //   if (confirm()) {
-      //     originalGsftSubmit.apply(null, arguments);
-      //   }
-      // };
     }
   }, 500);
 
